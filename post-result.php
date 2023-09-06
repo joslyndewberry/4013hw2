343cf1509563072c26a890c41863e20328525699
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-09-06 14:21:31 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -11,7 +11,7 @@ include "view-footer.php";
 
 function getDisplay() {
      if (isset($_POST['my-name'])) {
-  return "<p>The value set is:</p>" . echo $_POST['my-name'];
+  return "<p>The value set is:</p>" '.' echo $_POST['my-name'];
 } else {
 return "<p>Nothing posted to the page</p>";
 } 
