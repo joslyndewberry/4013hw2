1e1292cd59e495b60790d88b7ec8f5e34a220dd0
joslyndewberry <60158127+joslyndewberry@users.noreply.github.com>
2023-09-06 14:00:10 -0500

    Create post.php

---
+++ post.php
@@ -0,0 +1,14 @@
+<?php
+$pageTitle = "Post";
+include "view-header.php";
+?>
+      
+    <h1>Post</h1>
+<Form method="post" action="post-result.php">
+  <input type="text" name="my-name">
+  <input type="submit" value="Post data">
+</Form>
+
+<?php
+include "view-footer.php"
+?>
